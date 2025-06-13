8687125ec11167fb0e3369126fcc658f6487be45
seval2016 <sevalsenturk2016@gmail.com>
2025-06-14 01:28:09 +0300

    categories,policy,products,campains

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import Footer from "./components/Layout/Footer/Footer";
 import Policy from "./components/Policy/Policy";
 import Sliders from "./components/Slider/Sliders";
 import Categories from "./components/Categories/Categories";
-
+import Products from "./components/Products/Products";
+import Campaigns from "./components/Campains/Campaigns";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Header />
       <Sliders />
       <Categories />
-
+      <Products />
+      <Campaigns />
       <Policy />
       <Footer />
     </>
