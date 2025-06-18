f3f48cdc56c264be37921311e761025ce8c1d683
seval2016 <sevalsenturk2016@gmail.com>
2025-06-18 15:26:43 +0300

    cartItems length

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -31,6 +31,9 @@ PrevBtn.propTypes = {
 
 const Products = () => {
   const [products] = useState(productsData);
+  const [cartItems, setCartItems] = useState([]);
+
+  //console.log(cartItems.length);
 
   const sliderSettings = {
     dots: false,
@@ -67,7 +70,7 @@ const Products = () => {
         <div className="product-wrapper product-carousel">
           <Slider {...sliderSettings}>
             {products.map((product) => (
-              <ProductItem product={product} key={product.id} />
+              <ProductItem productItem={product} setCartItems={setCartItems} key={product.id} />
             ))}
           </Slider>
         </div>
