e6e860b26ab038a8dbed1031cc3940f320bac5b5
seval2016 <sevalsenturk2016@gmail.com>
2025-06-20 02:01:14 +0300

    mongo db added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log("Server is starting...");
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express(); 
+const port = 5000;
+const connect= async () => {
+    try {
+        await mongoose.connect('mongodb+srv://e-commerce:mern12345@mern-e-commerce.qgqnhh8.mongodb.net/');
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Express.js!');
+});
+
+app.listen(port, () => {
+    connect();
+    console.log(`Server is running at http://localhost:${port}`);
+    }   
+);
