27f5138f8d138f1d73ab02e2d513a4c08b3bf0a3
seval2016 <sevalsenturk2016@gmail.com>
2025-06-20 22:53:05 +0300

    category end points

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Category = require("../models/Category.js");
 
+// Yeni bir kategori oluşturma (Create)
 router.post("/", async (req, res) => {
   try {
     const { name, img } = req.body;
@@ -10,7 +11,6 @@ router.post("/", async (req, res) => {
     await newCategory.save();
 
     res.status(201).json(newCategory);
-    
   } catch (error) {
     console.log(error);
   }
@@ -18,7 +18,69 @@ router.post("/", async (req, res) => {
 
 // Tüm kategorileri getirme (Read- All)
 router.get("/", async (req, res) => {
-  res.send("Kategoriler getirildi!");
+  try {
+    const categories = await Category.find();
+
+    res.status(200).json(categories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Kategori ID'sine göre kategori getirme (Read - One)
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.categoryId);
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+
+    res.status(200).json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Kategori güncelleme (Update)
+router.put("/:categoryId", async (req, res) =>
+  {
+    try {
+      const category = await Category.findById(req.params.categoryId);
+      if (!category) {
+        return res.status(404).json({ error: "Category not found." });
+      }
+
+      const updatedCategory = await Category.findByIdAndUpdate(
+        req.params.categoryId,
+        req.body,
+        { new: true }
+      );
+
+      res.status(200).json(updatedCategory);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Server error." });
+    }
+  });
+
+// Kategori silme (Delete)
+router.delete("/:categoryId", async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.categoryId);
+    if (!category) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+
+    await Category.findByIdAndDelete(req.params.categoryId);
+
+    res.status(200).json({ message: "Category deleted successfully." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
 });
 
 module.exports = router;
