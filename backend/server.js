d2b229803ac59c231bbbda5c53951bf7d554ded6
seval2016 <sevalsenturk2016@gmail.com>
2025-06-20 16:11:21 +0300

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const app = express(); 
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const mainRoute = require("./routes/index.js");
 const port = 5000;
 
 dotenv.config();
 
-const connect= async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        throw error;
-    }
-}
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDb");
+  } catch (error) {
+    throw error;
+  }
+};
 
-app.get('/', (req, res) => {
-  res.send('Hello Express.js!');
-});
+app.use("/api", mainRoute);
 
 app.listen(port, () => {
-    connect();
-    console.log(`Server is running at http://localhost:${port}`);
-    }   
-);
+  connect();
+  console.log(`Sunucu ${port} portunda çalışıyor.`);
+});
