2279fa32d023205627dd6ad24c2ef609cee06b3c
seval2016 <sevalsenturk2016@gmail.com>
2025-06-22 01:23:51 +0300

    auth model

---
+++ backend/routes/auth.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/User.js");
+
+// Random Avatar URL'sini oluştur
+const generateAvatar = () => {
+  const randomAvatar = Math.floor(Math.random() * 70 + 1);
+  return `https://i.pravatar.cc/300?img=${randomAvatar}`;
+};
+
+
+
+// Kullanıcı Oluşturma (Create - Register)
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      avatar : generateAvatar(),
+    });
+
+    await newUser.save();
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+module.exports = router;
