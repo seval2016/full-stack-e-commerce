1fad68507c81ea978659fc488dc28f778ed2becd
seval2016 <sevalsenturk2016@gmail.com>
2025-06-28 23:13:27 +0300

    Creating an admin user page and get all user API

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,12 +5,14 @@ const router = express.Router();
 const categoryRoute = require("./categories.js");
 const authRoute = require("./auth.js");
 const productRoute = require("./products.js");
-const couponRoute = require("./coupon.js");
+const couponRoute = require("./coupons.js");
+const userRoute = require("./users.js");
 
 // Her rotayı ilgili yol altında kullanıyoruz
 router.use("/categories", categoryRoute);
 router.use("/auth", authRoute);
 router.use("/products", productRoute);
 router.use("/coupons", couponRoute);
+router.use("/users", userRoute);
 
 module.exports = router;
